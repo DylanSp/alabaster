4cca8684a3fab3eab6f57c4f8ff2bf0d2de890b0
Dylan Sprague <dsprague@docuphase.com>
2019-04-19 15:46:09 -0400

    Implement parsing (except for operations)

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,51 @@
 module Main where
 
+import Data.List
+import Text.Parsec
+
+import Parser
+
+import Eval
+import Syntax
+import Type
+
 main :: IO ()
 main = do
-    putStrLn "hello world"
+  putStrLn "Input:"
+  input <- getLine
+  let prog = runParse input
+  putStrLn $ "Parsed"
+  let typeChecked = typeCheck <$> prog
+  putStrLn $ "Typecheck result: " ++ (showEither typeChecked)
+  let evalResult = evalProgram <$> prog
+  putStrLn $ "Eval result: " ++ show evalResult
+  putStrLn "done"
+
+runParse :: String -> Either String Program
+runParse input = case (parse program "" input) of
+  Left err -> Left $ show err
+  Right ast -> Right ast
+
+typeCheck :: Program -> String
+typeCheck (Program decls entrypoint) = (unwords $ intersperse "\n" (checkDecl <$> decls)) ++ "\n" ++ showEither (typeOf entrypoint)
+
+checkDecl :: Decl -> String
+checkDecl (ident, expression) = "Type of " ++ ident ++ " is " ++ (show (typeOf expression))
+
+showEither :: (Show a, Show b) => Either a b -> String
+showEither (Left str) = show str
+showEither (Right str) = show str 
+
+{-
+typeCheck :: CoreExpr -> Either String String
+typeCheck ast = case (typeOf ast) of
+  Left typeErr -> show typeErr
+  Right rootType -> show rootType
+-}
+
+
+fromRight (Right (Program _ entry)) = entry
+
+prog1Text = "\\x : a -> x"
+
+prog2Text = "(\\x : a -> x) 1"
---
+++ src/Parser.hs
@@ -0,0 +1,166 @@
+module Parser where
+
+import Text.Parsec
+import qualified Text.Parsec.Token as Tok
+
+import Syntax
+
+type Parser a = Parsec String () a
+
+
+{- Lexer -}
+
+
+langDef :: Tok.LanguageDef ()
+langDef = Tok.LanguageDef
+  { Tok.commentStart    = ""
+  , Tok.commentEnd      = ""
+  , Tok.commentLine     = ""
+  , Tok.nestedComments  = False
+  , Tok.identStart      = letter
+  , Tok.identLetter     = alphaNum
+  , Tok.opStart         = oneOf "+-*="
+  , Tok.opLetter        = oneOf "="
+  , Tok.reservedNames   = ["let", "in", "fix", "true", "false", "if", "then", "else", "Int", "Bool"]
+  , Tok.reservedOpNames = ["+", "-", "*", "==", "\\", ":", "->"]
+  , Tok.caseSensitive   = True
+  }
+
+lexer :: Tok.TokenParser ()
+lexer = Tok.makeTokenParser langDef
+
+parens :: Parser a -> Parser a
+parens = Tok.parens lexer
+
+identifier :: Parser String
+identifier = Tok.identifier lexer
+
+symbol :: String -> Parser String
+symbol = Tok.symbol lexer
+
+integer :: Parser Integer
+integer = Tok.integer lexer
+
+semi :: Parser String
+semi = Tok.semi lexer
+
+whiteSpace :: Parser ()
+whiteSpace = Tok.whiteSpace lexer
+
+
+{- Parser -}
+
+
+program :: Parser Program
+program = do
+  decls <- many decl
+  entrypoint <- expr
+  return $ Program decls entrypoint
+
+decl :: Parser Decl
+decl = do
+  _ <- symbol "let"
+  lhs <- identifier
+  _ <- symbol "="
+  rhs <- expr
+  _ <- semi
+  return (lhs, rhs)
+
+-- includes application, which has to be handled separately because of left-recursion
+expr :: Parser CoreExpr
+expr =  {-parens expr 
+    <|> -}nonAppExpr `chainl1` (return Apply)
+
+nonAppExpr :: Parser CoreExpr
+nonAppExpr =
+        (parens expr)
+    <|> (Var <$> identifier)
+    <|> termLiteral
+    <|> lambdaExpr
+    <|> letExpr
+    <|> ifExpr
+    <|> opExpr
+    <|> fixExpr
+
+    {-
+applyExpr :: Parser CoreExpr
+applyExpr = do
+  func <- expr
+  arg <- expr
+  return $ Apply func arg
+-}
+
+lambdaExpr :: Parser CoreExpr
+lambdaExpr = do
+  _ <- symbol "\\"
+  argName <- identifier
+  _ <- symbol ":"
+  argType <- typeExpr
+  _ <- symbol "->"
+  body <- expr
+  return $ Lambda argName argType body
+
+letExpr :: Parser CoreExpr
+letExpr = do
+  _ <- symbol "let"
+  lhs <- identifier
+  _ <- symbol "="
+  rhs <- expr
+  _ <- symbol "in"
+  body <- expr
+  return $ Let lhs rhs body
+
+ifExpr :: Parser CoreExpr
+ifExpr = do
+  _ <- symbol "if"
+  cond <- expr
+  _ <- symbol "then"
+  trueExpr <- expr
+  _ <- symbol "else"
+  falseExpr <- expr
+  return $ If cond trueExpr falseExpr
+
+{-
+opExpr :: Parser CoreExpr
+opExpr = expr `chainl1` (Op <$> operation)
+-}
+
+opExpr = do
+  lhs <- expr
+  op <- operation
+  rhs <- expr
+  return $ Op op lhs rhs
+
+
+operation :: Parser BinOp
+operation =  (symbol "+" >> return Add)
+         <|> (symbol "-" >> return Subtract)
+         <|> (symbol "*" >> return Multiply)
+         <|> (symbol "==" >> return Equals)
+
+fixExpr :: Parser CoreExpr
+fixExpr = do
+  _ <- symbol "fix"
+  fixedExpr <- expr
+  return $ Fix fixedExpr
+
+typeExpr :: Parser Type
+typeExpr =  TVariable <$> identifier
+        <|> TConstructor <$> typeLiteral
+--        <|> typeFunction
+
+typeFunction :: Parser Type
+typeFunction = do
+  func <- typeExpr
+  arg <- typeExpr
+  return $ TFunction func arg
+
+typeLiteral :: Parser String
+typeLiteral =  symbol "Int"
+           <|> symbol "Bool"
+
+termLiteral :: Parser CoreExpr
+termLiteral = Lit <$> (  (LInt <$> integer)
+                 <|> (symbol "true" >> (return $ LBool True))
+                 <|> (symbol "false" >> (return $ LBool False))
+                  )
